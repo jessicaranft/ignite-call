988b925928f58955ef100d2fa647e02528b8eba7
Jessica Ranft <jessica.ranft@gmail.com>
2023-11-23 11:19:28 -0300

    fix: added eslint exceptions and postinstall script

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 import { prisma } from '@/lib/prisma'
-import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handle(
